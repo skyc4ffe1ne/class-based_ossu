9d6755933164b5e5a6eb82eb9e13ef549bc3d356
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-01-13 16:45:30 +0100

    remove duplicate Lab2

---
+++ Assignment-2/src/Pictures.java
@@ -0,0 +1,24 @@
+class Shape
+{
+  String kind;
+  int size;
+  
+  Shape(String kind, int size)
+  {
+    this.kind = kind;
+    this.size = size;
+  }
+}
+
+class Combo 
+{
+  String kind;
+  int size;
+  
+  Combo(String kind, int size)
+  {
+    this.kind = kind;
+    this.size = size;
+  }
+  
+}
