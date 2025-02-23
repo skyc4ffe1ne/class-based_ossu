f7d527d53d244ce7ff03366aa5b178ab78ae6fcb
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-02-23 22:39:07 +0100

    Added abstract class ABook for lift fields

--- Lab4/src/Library.java
+++ Lab4/src/Library.java
@@ -1,33 +1,38 @@
 interface IBook{}
 
-class Book implements IBook {
-  String title, author;
-  int dayTaken;
-  
-  Book(String title, String author, int dayTaken){
-    this.title = title;
-    this.author = author;
-    this.dayTaken = dayTaken; 
-  }
-}
-
-class RefBook implements IBook {
+abstract class ABook implements IBook{
   String title;
   int dayTaken;
   
-  RefBook(String title, int dayTaken){
+  ABook(String title, int dayTaken){
     this.title = title;
-    this.dayTaken = dayTaken; 
+    this.dayTaken = dayTaken;
+  }
+
+}
+
+class Book extends ABook {
+  String author;
+  
+  Book(String title, String author, int dayTaken){
+   super(title,dayTaken);
+   this.dayTaken = dayTaken; 
   }
 }
 
-class AudioBook implements IBook {
-  String title, author;
-  int dayTaken;
+class RefBook extends ABook {
+ 
+  RefBook(String title, int dayTaken){
+    super(title,dayTaken);
+ }
+
+}
+
+class AudioBook extends ABook {
+  String author;
   
   AudioBook(String title, String author, int dayTaken){
-    this.title = title;
-    this.author = author;
+    super(title,dayTaken);
     this.dayTaken = dayTaken; 
   }
 }
