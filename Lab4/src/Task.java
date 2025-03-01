836b8fb9cb904d1a30fcf0c70bf707453e47259c
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-03-01 13:45:37 +0100

    Added hasCycle method

--- Lab4/src/Task.java
+++ Lab4/src/Task.java
@@ -1,12 +1,4 @@
-
 import tester.*;
-
-interface IPre{
-  boolean hasCycle();
-  boolean helperHasCycle(int id);
-  boolean hasId(int id);
-}
-
 
 class Task {
  int id;  
@@ -17,11 +9,22 @@ class Task {
     this.pre = pre;
  }
 
-  boolean hasCycle()
+  boolean hasCycle(ITask tasks)
   {
-    return pre.hasCycle();
+    return pre.hasCycle(id, tasks);
+  }
+  
+  int getTask() {
+    return pre.getPre();
   }
 } 
+
+interface IPre{
+  boolean hasCycle(int id, ITask tasks);
+  boolean helperHasCycle(int id, ITask tasks);
+  boolean hasId(int id);
+  int getPre();
+}
 
 class ConsPre implements IPre{
   int first;
@@ -32,54 +35,63 @@ class ConsPre implements IPre{
     this.rest = rest;
   }
   
-  
-  public boolean hasCycle()
-  {
-    return this.rest.helperHasCycle(this.first);
+  public boolean hasCycle(int firstId, ITask tasks) {
+    return  this.helperHasCycle(firstId, tasks);
   }
   
-  public boolean helperHasCycle(int id)
-  {
-    if (this.hasId(id)) {
+  public boolean helperHasCycle(int firstId, ITask tasks){
+    if(tasks.getTask() == firstId){
       return true;
     }else {
-      return this.rest.helperHasCycle(this.first);
+      return this.rest.helperHasCycle(firstId, tasks);
     }
   }
   
-
-public boolean hasId(int id)
-{
-  if(this.first == id) {
-    return true;
-  }else {
-    return this.rest.hasId(id);
+  public int getPre()
+  {
+    return this.first;
   }
-}
+
+  
+
+  public boolean hasId(int id)
+  {
+    if(this.first == id) {
+      return true;
+    }else {
+      return this.rest.hasId(id);
+    }
+  }
 
 }
 
 class MtLoPre implements IPre{
 MtLoPre(){}
 
-public boolean hasCycle()
-{
-  return false; 
-}
-
-public boolean helperHasCycle(int id)
+  public boolean hasCycle(int id , ITask tasks)
   {
-    return false;
+    return false; 
   }
+
+  public boolean helperHasCycle(int id, ITask tasks)
+    {
+
+      return false;
+    }
   
-  public boolean hasId(int id) {
+   public boolean hasId(int id) {
     return false;
-  }
+   }
+
+   public int getPre() {
+    return 0; 
+   }
 }
 
 
 interface ITask{
  boolean hasCycle();
+ int getTask();
 }
 
 class ConsTask implements ITask{
@@ -90,10 +102,21 @@ class ConsTask implements ITask{
     this.first = first;
     this.rest = rest;
   }
-  
-  public boolean hasCycle() {
-    return first.hasCycle();
+
+  public boolean hasCycle()
+  {
+    if (this.first.hasCycle(this.rest)) { 
+      return true;
+    }else {
+      return this.rest.hasCycle();
+    } 
   }
+  
+  public int getTask() {
+    return first.getTask(); 
+  }
+  
+  
 
 }
 
@@ -105,6 +128,11 @@ class MtLoTask implements ITask{
   {
     return false;
   }
+ 
+  public int getTask() {
+    return 0;
+  }
+
 }
 
 class ExamplesTask{
@@ -165,13 +193,35 @@ class ExamplesTask{
   ITask lt_3 = new ConsTask(t_5, new ConsTask(t_6, emptyTask));
 
   
+  Task t_7 = new Task(8, new ConsPre(9, emptyPre));
+  Task t_8 = new Task(9, new ConsPre(10, emptyPre));
+  IPre pre_4 = new ConsPre(11, emptyPre);
+  Task t_9 = new Task(10, pre_4);
+  IPre pre_5 = new ConsPre(12,emptyPre);
+  Task t_10 = new Task(11, pre_5);
+  Task t_11 = new Task(12, new ConsPre(13, emptyPre));
+  Task t_12 = new Task(13, new ConsPre(12, emptyPre));
+
+  ITask lt_4 = 
+      new ConsTask(t_7,
+          new ConsTask(t_8,
+              new ConsTask(t_9,
+                  new ConsTask(t_10,
+                      new ConsTask(t_11,
+                          new ConsTask(t_12,
+                                   emptyTask))))));
+      
+
   // Test the method hasCycle
   boolean testHasCycle(Tester t) {
     return 
-        t.checkExpect(pre_0.hasCycle(), false)
+        t.checkExpect(lt_3.hasCycle(), true)
         &&
-        t.checkExpect(pre_1.hasCycle(), false)
+        t.checkExpect(lt_1.hasCycle(), false)
         &&
-        t.checkExpect(lt_3.hasCycle(), true);
+        t.checkExpect(lt_2.hasCycle(), false)
+        &&
+        t.checkExpect(lt_4.hasCycle(), true)
+        ;
   }
 }
