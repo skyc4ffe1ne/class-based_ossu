45ed10dc01feb960b23ac7d0c0715b1969265ffe
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-03-05 11:13:00 +0100

    Designed the world

--- Lab5/src/Game.java
+++ Lab5/src/Game.java
@@ -35,14 +35,18 @@ class MyPosn extends Posn {
 class Circle {
   MyPosn position; // in pixels
   MyPosn velocity; // in pixels/tick
+  int size; 
+  Color color; 
   
-  Circle(MyPosn position, MyPosn velocity){
+  Circle(MyPosn position, MyPosn velocity,int size, Color color){
     this.position = position;
     this.velocity = velocity;
+    this.size = size;
+    this.color = color;
   }
   
   Circle move() {
-    return new Circle(this.position.add(this.velocity), this.velocity);
+    return new Circle(this.position.add(this.velocity), this.velocity, this.size, this.color);
   }
   
   boolean isOffScreen(int widthScreen, int heightScreen) {
@@ -50,7 +54,7 @@ class Circle {
   }
   
   WorldImage draw() {
-    return  new CircleImage(30, OutlineMode.SOLID, Color.RED);
+    return  new CircleImage(this.size, OutlineMode.SOLID, this.color);
   }
   
   WorldScene place(WorldScene s) {
@@ -107,6 +111,44 @@ class MtLoCircle implements ILoCircle{
   }
 }
 
+class MyWorld extends World {
+ int width, height;
+ ILoCircle circles;
+
+  MyWorld(int width, int height, ILoCircle circles){
+    this.width = width;
+    this.height = height;
+    this.circles = circles;
+  }
+
+  public WorldScene makeScene(){
+    return circles.placeAll(new WorldScene(500,500));
+  }
+  
+  public WorldEnd worldEnds() {
+    if(false) {
+      return new WorldEnd(true, this.makeScene());
+    }else {
+      return new WorldEnd(false, this.makeScene());
+    }
+  }
+  
+   public World onMouseClicked(Posn pos) {
+     Circle anotherCircle =  new Circle(new MyPosn(pos),new MyPosn(0,5), 40, Color.YELLOW);
+     ILoCircle addCircle = new ConsLoCircle(anotherCircle, this.circles); 
+     return new MyWorld(this.width, this.height, addCircle);
+   } 
+   
+   public World onTick() {
+     return new MyWorld(this.width, this.height, this.circles.moveAll()); 
+   }  
+
+   public boolean bigBang() {
+     return bigBang(width, height, 1.0/28.0);
+   }
+  
+}
+
 class ExamplesGame{
   ExamplesGame(){}
 
@@ -121,10 +163,11 @@ class ExamplesGame{
   MyPosn p_3_velocity = new MyPosn(0,7);
 
   
-  Circle c_0 = new Circle(p_0,p_0_velocity);
-  Circle c_1 = new Circle(p_1,p_1_velocity);
-  Circle c_2 = new Circle(p_2,p_2_velocity);
-  Circle c_3 = new Circle(p_3,p_3_velocity); 
+  Circle c_0 = new Circle(p_0,p_0_velocity, 30, Color.RED);
+  Circle c_1 = new Circle(p_1,p_1_velocity, 40, Color.BLUE);
+  Circle c_2 = new Circle(p_2,p_2_velocity, 50, Color.GREEN);
+  Circle c_3 = new Circle(p_3,p_3_velocity, 60, Color.CYAN); 
+
   
   
   ILoCircle emptyList = new MtLoCircle();
@@ -188,13 +231,13 @@ class ExamplesGame{
  // Test method move 
   boolean testMove(Tester t) {
     return
-        t.checkExpect(c_0.move(), new Circle(new MyPosn(105, 100), p_0_velocity))
+        t.checkExpect(c_0.move(), new Circle(new MyPosn(105, 100), p_0_velocity, c_0.size, c_0.color))
         &&
-        t.checkExpect(c_1.move(), new Circle(new MyPosn(150, 255), p_1_velocity)) 
+        t.checkExpect(c_1.move(), new Circle(new MyPosn(150, 255), p_1_velocity, c_1.size, c_1.color)) 
         &&
-        t.checkExpect(c_2.move(), new Circle(new MyPosn(17, 57), p_2_velocity)) 
+        t.checkExpect(c_2.move(), new Circle(new MyPosn(17, 57), p_2_velocity, c_2.size, c_2.color)) 
         &&
-        t.checkExpect(c_3.move(), new Circle(new MyPosn(48, 36), p_3_velocity)) 
+        t.checkExpect(c_3.move(), new Circle(new MyPosn(48, 36), p_3_velocity, c_3.size, c_3.color)) 
         ;
   }
   
@@ -202,58 +245,64 @@ class ExamplesGame{
   return
       t.checkExpect(loC_0.moveAll(), 
           new ConsLoCircle(
-              new Circle(new MyPosn(105, 100), p_0_velocity),
-                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity)
-                    , emptyList)))
+              new Circle(new MyPosn(105, 100), p_0_velocity, c_0.size, c_0.color),
+                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity, c_1.size, c_1.color),
+                    emptyList)))
       &&
       t.checkExpect(loC_1.moveAll(), 
           new ConsLoCircle(
-              new Circle(new MyPosn(105, 100), p_0_velocity),
-                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity),
-                    new ConsLoCircle(new Circle(new MyPosn(17, 57), p_2_velocity),
+              new Circle(new MyPosn(105, 100), p_0_velocity, c_0.size, c_0.color),
+                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity, c_1.size, c_1.color),
+                    new ConsLoCircle(new Circle(new MyPosn(17, 57), p_2_velocity, c_2.size, c_2.color),
                         emptyList))))
       &&
       t.checkExpect(loC_2.moveAll(), 
           new ConsLoCircle(
-              new Circle(new MyPosn(105, 100), p_0_velocity),
-                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity),
-                    new ConsLoCircle(new Circle(new MyPosn(17, 57), p_2_velocity),
-                        new ConsLoCircle(new Circle(new MyPosn(48, 36), p_3_velocity),
+              new Circle(new MyPosn(105, 100), p_0_velocity, c_0.size, c_0.color),
+                new ConsLoCircle(new Circle(new MyPosn(150, 255), p_1_velocity, c_1.size, c_1.color),
+                    new ConsLoCircle(new Circle(new MyPosn(17, 57), p_2_velocity, c_2.size, c_2.color),
+                        new ConsLoCircle(new Circle(new MyPosn(48, 36), p_3_velocity, c_3.size, c_3.color),
                             emptyList)))))
       ;
   }
   
-  boolean testDrawCircle(Tester t) {
-    WorldCanvas c = new WorldCanvas(500, 500);
-    WorldScene s = new WorldScene(500, 500);
-
-    int ORIGINPOINTX = 500 / 2;
-    int ORIGINPOINTY = 500 / 2;
-
-    return 
-        c.drawScene(s.placeImageXY(c_0.draw(), ORIGINPOINTX, ORIGINPOINTY))
-        && c.show()
-       ;
-  } 
+//  boolean testDrawCircle(Tester t) {
+//    WorldCanvas c = new WorldCanvas(500, 500);
+//    WorldScene s = new WorldScene(500, 500);
+//
+//    int ORIGINPOINTX = 500 / 2;
+//    int ORIGINPOINTY = 500 / 2;
+//
+//    return 
+//        c.drawScene(s.placeImageXY(c_0.draw(), ORIGINPOINTX, ORIGINPOINTY))
+//        && c.show()
+//       ;
+//  } 
+//  
+//  boolean testPlace(Tester t) {
+//    WorldCanvas c = new WorldCanvas(500, 500);
+//    WorldScene s = new WorldScene(500, 500);
+//
+//    return 
+//        c.drawScene(c_0.place(s))
+//        && c.show()
+//       ;
+//  } 
+//  
+//  boolean testPlaceAll(Tester t) {
+//    WorldCanvas c = new WorldCanvas(500, 500);
+//    WorldScene s = new WorldScene(500, 500);
+//
+//    return 
+//        c.drawScene(loC_2.placeAll(s))
+//        && c.show()
+//       ;
+//  } 
   
-  boolean testPlace(Tester t) {
-    WorldCanvas c = new WorldCanvas(500, 500);
-    WorldScene s = new WorldScene(500, 500);
+    boolean testBigBang(Tester t) {
+      MyWorld w = new MyWorld(500,500, loC_2);
+      return w.bigBang();
+    }
 
-    return 
-        c.drawScene(c_0.place(s))
-        && c.show()
-       ;
-  } 
-  
-  boolean testPlaceAll(Tester t) {
-    WorldCanvas c = new WorldCanvas(500, 500);
-    WorldScene s = new WorldScene(500, 500);
-
-    return 
-        c.drawScene(loC_2.placeAll(s))
-        && c.show()
-       ;
-  } 
 }
  
